f1e8877fd8677a92f136293a29ab06f385b3fdd7
Chirag Jain <chiragjain78382@gmail.com>
2025-05-24 01:59:13 +0530

    betterAuth

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,7 @@ import { Hono } from 'hono'
 import { logger } from 'hono/logger'
 import { expensesRoute } from './routes/expenses'
 import { cors } from 'hono/cors'
-import { HTTPException } from 'hono/http-exception'
+// import { HTTPException } from 'hono/http-exception'
 
 const app = new Hono().basePath('/api')
 
